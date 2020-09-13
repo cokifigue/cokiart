d4d71b71485607baa550119065c499ae25b0e0de
Constanza <cokifigue@yahoo.com>
2020-09-13 16:22:18 -0400

    Updates

---
+++ precache-manifest.a34a2a1a403f30621df8994f9ded0b2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fec0fb5e6207af51817b9730f7dad70",
+    "url": "/cokiart/index.html"
+  },
+  {
+    "revision": "347a0b4f6cbdeceace3c",
+    "url": "/cokiart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fb709cf86f2494ea5cb",
+    "url": "/cokiart/static/js/2.5d9dddf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cokiart/static/js/2.5d9dddf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347a0b4f6cbdeceace3c",
+    "url": "/cokiart/static/js/main.3cb8657c.chunk.js"
+  },
+  {
+    "revision": "aea5fa47d79d1b7be8cd",
+    "url": "/cokiart/static/js/runtime-main.c45a8f6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cokiart/static/media/logo.5d5d9eef.svg"
+  }
+]);
